46662ec611b8d6e5c7b938ca1895be7cba5bab2e
Savannah Jackson <Boisterouscoder@gmail.com>
2022-09-20 18:02:20 -0500

    removing -r from server build
    this is a test

--- server-build.sh
+++ server-build.sh
@@ -1,11 +1,8 @@
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 wasm-pack build --target web
-cd ./pkg
-ls
-cd ..
-cp -r ./pkg/yakking_yak.js ./public/bin/yakking_yak.js
-cp -r ./pkg/yakking_yak_bg.wasm ./public/bin/yakking_yak_bg.wasm
+cp ./pkg/yakking_yak.js ./public/bin/yakking_yak.js
+cp ./pkg/yakking_yak_bg.wasm ./public/bin/yakking_yak_bg.wasm
 npx browserify src/js/start.js -o public/bin/main.js
 npx pug ./src/pug -o ./public
-cp -r ./src/css/style.css ./public/bin/style.css
+cp ./src/css/style.css ./public/bin/style.css
