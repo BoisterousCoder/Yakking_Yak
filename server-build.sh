d356456b15d000dfa57e13aab5440887a48889a6
Savannah Jackson <Boisterouscoder@gmail.com>
2022-09-20 18:10:24 -0500

    oops typo

--- server-build.sh
+++ server-build.sh
@@ -3,7 +3,7 @@ curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 wasm-pack build --target web
 cd public
 mkdir bin
-cd..
+cd ..
 cp ./pkg/yakking_yak.js ./public/bin/yakking_yak.js
 cp ./pkg/yakking_yak_bg.wasm ./public/bin/yakking_yak_bg.wasm
 npx browserify src/js/start.js -o public/bin/main.js
