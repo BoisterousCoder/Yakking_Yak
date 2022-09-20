65451ed8a771d2080dc2a10f0fb7edbcddc1a8d5
Savannah Jackson <Boisterouscoder@gmail.com>
2022-09-20 17:58:44 -0500

    another server build test

--- server-build.sh
+++ server-build.sh
@@ -1,6 +1,11 @@
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+wasm-pack build --target web
 cd ./pkg
 ls
 cd ..
-npm run build
+cp -r ./pkg/yakking_yak.js ./public/bin/yakking_yak.js
+cp -r ./pkg/yakking_yak_bg.wasm ./public/bin/yakking_yak_bg.wasm
+npx browserify src/js/start.js -o public/bin/main.js
+npx pug ./src/pug -o ./public
+cp -r ./src/css/style.css ./public/bin/style.css
